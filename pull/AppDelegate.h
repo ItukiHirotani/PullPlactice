d685665c628a8fbeadfd996d4b83267f825103f8
YuukiTanihara <wide.tani@gmail.com>
2016-06-17 10:48:56 +0900

    first

---
+++ pull/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pull
+//
+//  Created by Yuuki Tanihara on 2016/06/17.
+//  Copyright © 2016年 YuukiTanihara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ pull/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  pull
+//
+//  Created by Yuuki Tanihara on 2016/06/17.
+//  Copyright © 2016年 YuukiTanihara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
